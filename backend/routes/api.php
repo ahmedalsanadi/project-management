b98e8213fad5063d8b4fffef539fcc4d28822eb3
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2025-01-27 21:46:10 +0300

    feat(backend): add role-based api middlware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('tasks', TaskController::class);
+});
+
+//
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    // Route::apiResource('projects', ProjectController::class);
+});
+
+Route::middleware(['auth:sanctum', 'role:member'])->group(function () {
+    // Route::apiResource('tasks', TaskController::class);
+});
