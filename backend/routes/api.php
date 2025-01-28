d194bac88c051af5e239f3f6c2efc3f03bc2b196
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2025-01-28 17:07:18 +0300

    feat: Prepered Authenation api functionalities

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,25 +1,31 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::prefix('auth')->group(function () {
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('projects', ProjectController::class);
-    Route::apiResource('tasks', TaskController::class);
+    // Endpoint: /api/auth/register
+    Route::post('/register', [AuthController::class, 'register']);
+
+    // Endpoint: /api/auth/login
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
 
 //
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    // list admin routes:
     // Route::apiResource('projects', ProjectController::class);
 });
 
 Route::middleware(['auth:sanctum', 'role:member'])->group(function () {
-    // Route::apiResource('tasks', TaskController::class);
+    // List member routes :
+    //Route::apiResource('tasks', TaskController::class);
 });
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
